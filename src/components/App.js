12f0b948023cacebab5df60b52a193fecf810ac5
Nick Nauert <nicknauert@gmail.com>
2017-10-05 14:19:18 -0400

    init commit

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import '../styles/App.css';
+
+import BookList from '../containers/BookList';
+import BookDetail from '../containers/BookDetail';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Books N' Roses: Appetite for Instruction</h1>
+                <div className="row">
+                  <BookList/>
+                  <BookDetail/>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default App;
